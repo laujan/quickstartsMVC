4c2d4b194f5b5569919c41c0b329b657267b89be
la <v-laujan@microsoft.com>
2019-07-10 17:46:13 -0700

    First commit

---
+++ mvcQuickStartTabs/Controllers/ChannelGroupController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using mvcQuickStartTabs.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace mvcQuickStartTabs.Controllers
+{
+    [Route("channelgroup")]
+    public class ChannelGroupController : Controller
+    {
+        [Route("")]
+        public IActionResult ChannelGroup()
+        {
+           
+            return View();
+        }
+    }
+}
---
+++ mvcQuickStartTabs/Models/ChannelGroup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvcQuickStartTabs.Models
+{
+    public class ChannelGroup
+    {
+        public string Message { get; set; }
+
+        public string GetColor()
+        {
+            Message = "ChannelGroup.cs says: 'You picked";
+            return Message;
+        }
+    }
+}
+
